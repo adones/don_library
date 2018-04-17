b8d58a178f72917c5de071b3c7f2f9ab991951ac
Don Fuentes <adones.fuentes@gmail.com>
2018-04-17 14:26:21 +0400

    Release 0.0.1.

---
+++ Example/Pods/Target Support Files/ARNLibrary/ARNLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ARNLibrary : NSObject
+@end
+@implementation PodsDummy_ARNLibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-ARNLibrary_Tests/Pods-ARNLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARNLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ARNLibrary_Tests
+@end
